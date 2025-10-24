9d5363bb221531f3d991ad85d5fef7c9b5722671
josue <josue.patino@epn.edu.ec>
2025-10-23 20:03:59 -0500

    pregunta 6,7,8,9 y preguntas

--- src/EjemploLlamado.java
+++ src/EjemploLlamado.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class EjemploLlamado {
+    public static double raiz(double n) {
+        return Math.sqrt(n); // método estático
+    }
+
+    public void mostrar(double valor) {
+        System.out.println("Valor: " + valor); // método de instancia
+    }
+    public static void main(String[] args) {
+        double r = EjemploLlamado.raiz(25);
+        EjemploLlamado obj = new EjemploLlamado();
+        obj.mostrar(r);
+    }
 }
+
+
+
--- src/EjemploReturn.java
+++ src/EjemploReturn.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class EjemploReturn {
+    public static int sumar(int a, int b) {
+        return a + b; // devuelve un número
+    }
+
+    public static String saludar(String nombre) {
+        return "Hola " + nombre; // devuelve texto
+    }
+    public static void main(String[] args) {
+        System.out.println(sumar(3, 7));
+        System.out.println(saludar("Josue"));
+    }
 }
